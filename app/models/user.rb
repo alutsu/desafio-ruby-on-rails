c9fe6e6141f52baab02e605e3eb31c986debe633
João Victor <jovictor.miguel@gmail.com>
2020-08-28 12:38:43 -0300

    test: ensure User model are valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
+# Model for user informations
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validate user name' do
+    it 'should be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '123123', admin: true)
+
+      expect(user).to be_valid
+    end
+
+    it 'should not be valid' do
+      user = User.new(email: 'valid@mail.com', password: '123123', admin: true)
+
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'Validate user email' do
+    it 'should be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '123123', admin: true)
+
+      expect(user).to be_valid
+    end
+
+    it 'should not be valid' do
+      user = User.new(name: 'Valid name', password: '123123', admin: true)
+
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'Validate user password' do
+    it 'should be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '123123', admin: true)
+
+      expect(user).to be_valid
+    end
+
+    it 'should not be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '', admin: true)
+
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'Validate user permissions' do
+    it 'should be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '123123', admin: true)
+
+      expect(user).to be_valid
+    end
+
+    it 'should not be valid' do
+      user = User.new(name: 'Valid name', email: 'valid@mail.com', password: '123123')
+
+      expect(user).to_not be_valid
+    end
+  end
 end
