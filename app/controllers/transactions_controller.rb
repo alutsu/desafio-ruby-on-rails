90342b0e18a1f913b52e5ad320a79c0e788aecef
João Victor <jovictor.miguel@gmail.com>
2020-08-28 19:17:50 -0300

    feat: add pagination

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
     @q = Transaction.includes(:transaction_type, :store).ransack(params[:q])
-    @transactions = @q.result
+    @transactions = @q.result.page(params[:page])
   end
 end
