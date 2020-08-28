1426fd1b8e245453a9cf9e37dadd3623d6f7baa2
João Victor <jovictor.miguel@gmail.com>
2020-08-28 20:54:48 -0300

    refactor: change test controller

---
+++ spec/controllers/transactions_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require_relative '../support/devise'
+
+RSpec.describe TransactionsController, type: :controller do
+  context 'GET #index for logged user' do
+    login_user
+
+    it 'should success and render to index page' do
+      get :index
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  context 'GET #index for not logged user' do
+    it 'should not render index if user are not authenticated' do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
