91412845f0ef96c2bcd40a3977e3a955157958ac
João Victor <jovictor.miguel@gmail.com>
2020-08-29 11:04:26 -0300

    test: ensure TransactionHelper calc_balance is valid

---
+++ spec/helpers/transactions_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require_relative '../support/devise'
+
+RSpec.describe TransactionsHelper, type: :helper do
+  context 'Validate helper' do
+    it 'should return a float' do
+      create_list(:transaction, 10, :positive)
+      create_list(:transaction, 10, :negative)
+      transactions = Transaction.all
+      transactions.each do |transaction|
+        puts transaction.value
+      end
+
+      expect(helper.calc_balance(transactions)).to be_kind_of(Float)
+    end
+  end
+end
